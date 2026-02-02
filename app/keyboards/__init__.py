b15426e2cb489eb1cb2a2d99391c3d0f9840ef0d
1vansaw <ivansaw42@gmail.com>
2026-02-02 22:21:42 +0300

    Import workshops_schemes from electroshemes

--- app/keyboards/__init__.py
+++ app/keyboards/__init__.py
@@ -2,4 +2,5 @@
 from .main_menu import main, inline_main_menu
 from .admins_menu import admin_menu, auto_backup_menu, confirm_edit_admins, confirm_menu, markup, clear_chat, del_admins, backup_db_confirm_kb, source_keyboard
 from .editor_menu import edit_mashines, del_contact, del_users, del_machines, confirm_edit_mashines, confirm_edit_users, add_contact, personal_main
+from .electroshemes import workshops_schemes
 from .workshops import workshops, shops_1, shops_2, shops_3, shops_11, shops_15, shops_17, shops_20, shops_26, shops_kmt
